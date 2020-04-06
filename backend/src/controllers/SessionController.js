901f54c5c5ef3d146d73cae2aec975c1cb1659b0
viniciusalmeidadev <viniciusalmeidamail@gmail.com>
2020-04-05 23:20:41 -0300

    login de ong por username ou email, ao invés de ID

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -12,19 +12,54 @@ module.exports = {
         const { userId, userPassword } = req.body;
 
 
-        const ong = await connection('ongsCompleted')
-        .where('id', userId)
-        .select('name')
+        const login = await connection('ongsCompleted')
+        .where('ongName', userId)
+        .select(['name', 'id'])
         .first();
         
-
         
-        if(!ong){
-            return res.status(400).json({error: 'No Ong found with this Id'});
+        
+        if(!login){
+            const email = await connection('ongsCompleted')
+            .where('email', userId)
+            .select(['name', 'id'])
+            .first();
+
+           
+
+            if(!email){
+                return res.json({message: 'Login error: Username or E-mail is invalid!'})
+            }
+            
+            const {password} = await connection('ongsCompleted')
+            .where('id', email.id)
+            .select('password')
+            .first();
+        
+                
+        
+        
+            if(!await bcrypt.compare(userPassword, password)){
+                return res.status(400).json({error: 'Password Invalid'});
+            }
+        
+            const token = jwt.sign({id: email.id}, process.env.APP_SECRET);
+    
+            
+
+            const data = [
+                email.name,
+                email.id,
+                token
+            ]
+            
+            return res.json({data});
+    
         }
+        
 
         const {password} = await connection('ongsCompleted')
-        .where('id', userId)
+        .where('id', login.id)
         .select('password')
         .first();
     
@@ -35,10 +70,15 @@ module.exports = {
             return res.status(400).json({error: 'Password Invalid'});
         }
     
-        const token = jwt.sign({id: userId}, process.env.APP_SECRET);
+        const token = jwt.sign({id: login.id}, process.env.APP_SECRET);
 
+        const data = [
+            login.name,
+            login.id,
+            token
+        ]
         
-        return res.json({userId, ong, token});
+        return res.json({data});
 
         }
 }
